c66379adb7db7f845f709dac3f445e583d6cfe21
vcarvalho0 <vitor1.santos00@gmail.com>
2023-02-24 19:18:49 -0300

    feat: Add custom fonts

--- src/style/global.js
+++ src/style/global.js
@@ -17,8 +17,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Source Code Pro', sans-serif;
   }
 
   p {
