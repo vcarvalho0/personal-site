72cea4d9aa8766b4feb33ecf7379fa4fbbc5a7d5
vcarvalho0 <vitor1.santos00@gmail.com>
2023-03-01 17:26:26 -0300

    feat: Main page first structure

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,8 +1,14 @@
+import * as S from './style'
+import { Header } from '../../components/Header'
+import { Skills } from '../../components/Skills'
+import { Footer } from '../../components/Footer'
+
 export function Main() {
   return (
-    <>
-      <h1>Hey, welcome!</h1>
-      <h2>Here you can find some informations about me.</h2>
-    </>
+    <S.Container>
+      <Header />
+      <Skills />
+      <Footer />
+    </S.Container>
   )
 }
